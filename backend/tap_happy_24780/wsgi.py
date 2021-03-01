73e231392f7d4f9f6798d11efc1502902137a42a
Crowdbotics <team@crowdbotics.com>
2021-03-01 19:43:38 +0000

    Update models

--- backend/tap_happy_24780/wsgi.py
+++ backend/tap_happy_24780/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tap_happy_24780.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tap_happy_24780.settings")
 
 application = get_wsgi_application()
